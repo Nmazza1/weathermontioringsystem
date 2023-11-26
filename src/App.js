766a43e92ac673f7b899304a970251c5ff0f4d8a
Emil Vezina <vezinaemil@gmail.com>
2023-11-25 20:37:33 -0500

    auth
    atuh

--- src/App.js
+++ src/App.js
@@ -1,119 +1,143 @@
-import React, { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
 
 function App() {
-  
-  var data = [];
+ const [isLoggedIn, setIsLoggedIn] = useState(false);
+ const [username, setUsername] = useState('');
+ const [password, setPassword] = useState('');
 
-  
-  const [temperature, setTemperature] = useState(20);
-  const [humidity, setHumidity] = useState(50);
+ const handleUsernameChange = (event) => {
+  setUsername(event.target.value);
+ };
 
-  const handleTemperatureChange = (event) => {
-    const newTemperature = parseInt(event.target.value);
-    setTemperature(newTemperature);
-  };
+ const handlePasswordChange = (event) => {
+  setPassword(event.target.value);
+ };
 
-  const handleHumidityChange = (event) => {
-    const newHumidity = parseInt(event.target.value);
-    setHumidity(newHumidity);
-  };
-
-  const loadDataFromTable =  () => {
-
-    axios
-      .get('http://172.17.29.171:5000/api/temperature')
-      .then((response) => {
-        if (response.status == 200) {
-
-          const tempData = response.data;
-          data = tempData;
-        
-          console.log(response);
-        }
-      })
-      .catch((error) => {
-      })
+ const handleSubmit = (event) => {
+  event.preventDefault();
+  if (username === 'admin' && password === 'admin') {
+    setIsLoggedIn(true);
   }
+ };
 
-  useEffect(()=>{
-    console.log("Promise is being made")
-    loadDataFromTable();
-  },[])
+ var data = [];
 
-  let temperatureEmoji, humidityEmoji;
+ const [temperature, setTemperature] = useState(20);
+ const [humidity, setHumidity] = useState(50);
 
-  if (temperature > 24) {
-    temperatureEmoji = '🔥'; // Too hot
-  } else if (temperature < 18) {
-    temperatureEmoji = '❄️'; // Too cold
-  } else {
-    temperatureEmoji = '🌱'; // Ideal temperature for a plant
-  }
+ const handleTemperatureChange = (event) => {
+  const newTemperature = parseInt(event.target.value);
+  setTemperature(newTemperature);
+ };
 
-  if (humidity > 60) {
-    humidityEmoji = '💧'; // Too humid
-  } else if (humidity < 40) {
-    humidityEmoji = '🏜️'; // Too dry
-  } else {
-    humidityEmoji = '🌱'; // Ideal humidity for a plant
-  }
+ const handleHumidityChange = (event) => {
+  const newHumidity = parseInt(event.target.value);
+  setHumidity(newHumidity);
+ };
 
-  const emojiStyle = {
-    fontSize: '2em', // Adjust the font size here for larger emojis
-  };
+ const loadDataFromTable = () => {
+  axios
+    .get('http://172.17.29.171:5000/api/temperature')
+    .then((response) => {
+      if (response.status == 200) {
+        const tempData = response.data;
+        data = tempData;
+      }
+    })
+    .catch((error) => {
+    });
+ }
 
+ useEffect(() => {
+  console.log("Promise is being made");
+  loadDataFromTable();
+ },[])
+
+ if (!isLoggedIn) {
   return (
-    <div className="App">
-      <nav>
-        <ul>
-          <li><a href="/">Home</a></li>
-          <li><a href="/data">Data</a></li>
-        </ul>
-      </nav>
+    <form onSubmit={handleSubmit}>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={handleUsernameChange} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={handlePasswordChange} />
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
+ }
 
-      <h1>Weather Monitoring System</h1>
-      <div className="container">
-        <h2>Weather</h2>
-        <div className="weather-info">
-          <div className="weather-entry">
-            <span>Temperature: {temperature}°C</span>
-            <span style={emojiStyle} role="img" aria-label="Emoji">
-              {temperatureEmoji}
-            </span>
-          </div>
-          <div className="weather-entry">
-            <span>Humidity: {humidity}%</span>
-            <span style={emojiStyle} role="img" aria-label="Emoji">
-              {humidityEmoji}
-            </span>
-          </div>
+ let temperatureEmoji, humidityEmoji;
+
+ if (temperature > 24) {
+  temperatureEmoji = '🔥'; // Too hot
+ } else if (temperature < 18) {
+  temperatureEmoji = '❄️'; // Too cold
+ } else {
+  temperatureEmoji = '🌱'; // Ideal temperature for a plant
+ }
+
+ if (humidity > 60) {
+  humidityEmoji = '💧'; // Too humid
+ } else if (humidity < 40) {
+  humidityEmoji = '🏜️'; // Too dry
+ } else {
+  humidityEmoji = '🌱'; // Ideal humidity for a plant
+ }
+
+ const emojiStyle = {
+  fontSize: '2em', // Adjust the font size here for larger emojis
+ };
+
+ return (
+  <div className="App">
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/data">Data</a></li>
+      </ul>
+    </nav>
+
+    <h1>Weather Monitoring System</h1>
+    <div className="container">
+      <h2>Weather</h2>
+      <div className="weather-info">
+        <div className="weather-entry">
+          <span>Temperature: {temperature}°C</span>
+          <span style={emojiStyle} role="img" aria-label="Emoji">
+            {temperatureEmoji}
+          </span>
         </div>
-
-        {/* Input fields and buttons for temperature and humidity update */}
-        <div className="input-container">
-          <input
-            type="number"
-            value={temperature}
-            onChange={handleTemperatureChange}
-            placeholder="Temperature"
-          />
-          <input
-            type="number"
-            value={humidity}
-            onChange={handleHumidityChange}
-            placeholder="Humidity"
-          />
+        <div className="weather-entry">
+          <span>Humidity: {humidity}%</span>
+          <span style={emojiStyle} role="img" aria-label="Emoji">
+            {humidityEmoji}
+          </span>
         </div>
       </div>
 
-      <footer className="footer">
-        <p>&copy; BMV IOT2, 2023</p>
-      </footer>
+      {/* Input fields and buttons for temperature and humidity update */}
+      <div className="input-container">
+        <input
+          type="number"
+          value={temperature}
+          onChange={handleTemperatureChange}
+          placeholder="Temperature"
+        />
+        <input
+          type="number"
+          value={humidity}
+          onChange={handleHumidityChange}
+          placeholder="Humidity"
+        />
+      </div>
     </div>
-  );
+  </div>
+ );
 }
 
 export default App;
