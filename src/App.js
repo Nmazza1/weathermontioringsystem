4641da7cb910cfd574f53e4d3af7a22d9aa83467
Emil Vezina <vezinaemil@gmail.com>
2023-12-05 20:16:27 -0500

    auth recent data
    added auth and recent data

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [registerUsername, setRegisterUsername] = useState('');
+  const [registerPassword, setRegisterPassword] = useState('');
   const [temperature, setTemperature] = useState(20);
   const [humidity, setHumidity] = useState(50);
 
@@ -17,34 +19,47 @@ function App() {
     setPassword(event.target.value);
   };
 
+  const handleRegister = (event) => {
+  event.preventDefault();
+  axios.post('http://127.0.0.1:5000/api/user', {
+    username: registerUsername,
+    password: registerPassword
+  })
+  .then(response => {
+    if (response && response.data) {
+      alert(response.data.message);
+    }
+  })
+  .catch(error => {
+    if (error.response && error.response.data) {
+      alert(error.response.data.error);
+    } else {
+      console.error("An error occurred:", error);
+    }
+  });
+};
+
   const handleSubmit = (event) => {
     event.preventDefault();
-    if (username === 'admin' && password === 'admin') {
-      setIsLoggedIn(true);
-    }
-  };
+    axios.post('http://127.0.0.1:5000/api/login', { username, password })
+        .then(response => {
+            alert(response.data.message);
+            setIsLoggedIn(true);
+        })
+        .catch(error => alert(error.response.data.error));
+};
 
-  const handleTemperatureChange = (event) => {
-    setTemperature(parseInt(event.target.value, 10));
-  };
-
-  const handleHumidityChange = (event) => {
-    setHumidity(parseInt(event.target.value, 10));
-  };
-
-const loadDataFromTable = () => {
-  axios
-    .get('http://127.0.0.1:5000/api/temperature')
-    .then((response) => {
-      if (response.status === 200) {
+  const loadDataFromServer = () => {
+    axios
+      .get('http://127.0.0.1:5000/api/temperature')
+      .then((response) => {
+        if (response.status === 200) {
         // Parse the string into JSON
         const jsonData = JSON.parse(response.data);
         console.log("Fetched Data:", jsonData); // Log the fetched data
-
         if (Array.isArray(jsonData) && jsonData.length > 0) {
           const mostRecentData = jsonData[jsonData.length - 1];
           console.log("Most Recent Data:", mostRecentData); // Log the most recent data
-
           setTemperature(mostRecentData.temp);
           setHumidity(mostRecentData.humidity);
         } else {
@@ -55,48 +70,48 @@ const loadDataFromTable = () => {
     .catch((error) => {
       console.error("Error fetching data:", error);
     });
-};
+    };
 
   useEffect(() => {
-    loadDataFromTable();
+    loadDataFromServer();
   }, []);
 
   if (!isLoggedIn) {
     return (
-      <form onSubmit={handleSubmit}>
-        <label>
-          Username:
-          <input type="text" value={username} onChange={handleUsernameChange} />
-        </label>
-        <label>
-          Password:
-          <input type="password" value={password} onChange={handlePasswordChange} />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Username:
+            <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+          </label>
+          <input type="submit" value="Login" />
+        </form>
+
+        <form onSubmit={handleRegister}>
+          <h2>Register</h2>
+          <label>
+            Username:
+            <input type="text" value={registerUsername} onChange={e => setRegisterUsername(e.target.value)} />
+          </label>
+          <label>
+            Password:
+            <input type="password" value={registerPassword} onChange={e => setRegisterPassword(e.target.value)} />
+          </label>
+          <input type="submit" value="Register" />
+        </form>
+      </div>
     );
   }
 
-  let temperatureEmoji, humidityEmoji;
-
-  if (temperature > 24) {
-    temperatureEmoji = '🔥'; // Too hot
-  } else if (temperature < 18) {
-    temperatureEmoji = '❄️'; // Too cold
-  } else {
-    temperatureEmoji = '🌱'; // Ideal temperature for a plant
-  }
-
-  if (humidity > 60) {
-    humidityEmoji = '💧'; // Too humid
-  } else if (humidity < 40) {
-    humidityEmoji = '🏜️'; // Too dry
-  } else {
-    humidityEmoji = '🌱'; // Ideal humidity for a plant
-  }
+  let temperatureEmoji = temperature > 24 ? '🔥' : temperature < 18 ? '❄️' : '🌱';
+  let humidityEmoji = humidity > 60 ? '💧' : humidity < 40 ? '🏜️' : '🌱';
 
   const emojiStyle = {
-    fontSize: '2em', // Adjust the font size here for larger emojis
+    fontSize: '2em',
   };
 
   return (
@@ -107,21 +122,6 @@ const loadDataFromTable = () => {
           <li><a href="/data">Data</a></li>
         </ul>
       </nav>
-if (!isLoggedIn) {
-  return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <label className="login-label">
-        Username:
-        <input className="login-input" type="text" value={username} onChange={handleUsernameChange} />
-      </label>
-      <label className="login-label">
-        Password:
-        <input className="login-input" type="password" value={password} onChange={handlePasswordChange} />
-      </label>
-      <input className="login-submit-button" type="submit" value="Submit" />
-    </form>
-  );
-}
 
       <h1>Weather Monitoring System</h1>
       <div className="container">
@@ -129,32 +129,12 @@ if (!isLoggedIn) {
         <div className="weather-info">
           <div className="weather-entry">
             <span>Temperature: {temperature}°C</span>
-            <span style={emojiStyle} role="img" aria-label="Emoji">
-              {temperatureEmoji}
-            </span>
+            <span style={emojiStyle}>{temperatureEmoji}</span>
           </div>
           <div className="weather-entry">
             <span>Humidity: {humidity}%</span>
-            <span style={emojiStyle} role="img" aria-label="Emoji">
-              {humidityEmoji}
-            </span>
+            <span style={emojiStyle}>{humidityEmoji}</span>
           </div>
-        </div>
-
-        {/* Input fields and buttons for temperature and humidity update */}
-        <div className="input-container">
-          <input
-            type="number"
-            value={temperature}
-            onChange={handleTemperatureChange}
-            placeholder="Temperature"
-          />
-          <input
-            type="number"
-            value={humidity}
-            onChange={handleHumidityChange}
-            placeholder="Humidity"
-          />
         </div>
       </div>
       <footer className="footer">
